23edae73b64a4b7a9174ce48d7e7971636a98993
StaticCloud <diegomarrs@gmail.com>
2021-12-07 16:40:49 -0800

    added weather icons and changed lat and lon for testing reasons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var weekEl = $("#week");
 
 // api key and url
 var key = "6c824467b18388bbc515d17b0f0e72db";
-var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=45.5234&lon=-122.6762&units=imperial&appid=" + key;
+var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=46.729728&lon=-117.181843&units=imperial&appid=" + key;
 
 // fetch the url then parse the response into human readable data via JSON
 fetch(apiUrl).then(function(response) {
@@ -25,12 +25,20 @@ var displayTodaysWeather = function(day) {
     var today = unixConverter(day.dt);
     // create elements for the city and date
     var city = $("<h1>").text("Placeholder").addClass("card-title");
-    var date = $("<h3>").text((today.getMonth() + 1)
+
+    // get the weather icon
+    var iconUrl = "http://openweathermap.org/img/w/" + day.weather[0].icon + ".png";
+    // create an image element with that icon
+    var weatherIcon = $("<img>").attr("src", iconUrl).attr("height", "40px");
+    var date = $("<h3>").html((today.getMonth() + 1)
                             + "/" + today.getDate()
                             + "/" + today.getFullYear()).addClass("card-subtitle text-muted");
 
+    // append the icon and the date to a container so they can display on the same line
+    var dateContainer = $("<div>").css("display", "inline-flex").append(date).append(weatherIcon);
+
     // append our new elements to the todayEl
-    todayEl.append(city).append(date);
+    todayEl.append(city).append(dateContainer);
 
     // then, render the temp, humidity, wind, and uvi
     var temp = $("<p>").text("Temp: " + day.temp.max + "F").addClass("card-text");
@@ -51,14 +59,21 @@ var displayWeeklyWeather = function(week) {
         // repeat what we we did in displayTodaysWeather (might refactor later to avoid repetition) but for every day of the week
         var day = unixConverter(week[i].dt);
         var weekCard = $("<div>").addClass("card col p-2");
+
+        // get the weather icon to display with our date
+        var iconUrl = "http://openweathermap.org/img/w/" + week[i].weather[0].icon + ".png";
+        var weatherIcon = $("<img>").attr("src", iconUrl).attr("height", "40px");
+        
         var day = $("<h4>").text((day.getMonth() + 1)
                             + "/" + day.getDate()
                             + "/" + day.getFullYear()).addClass("card-title");
+        var dateContainer = $("<div>").css("display", "inline-flex").append(day).append(weatherIcon);
+
         var temp = $("<p>").text("Temp: " + week[i].temp.max + "F").addClass("card-text");
         var humidity = $("<p>").text("Humidity: " + week[i].humidity + "%").addClass("card-text");
         var wind = $("<p>").text("Wind: " + week[i].wind_speed + "MPH").addClass("card-text");
 
-        weekCard.append(day).append(temp).append(humidity).append(wind);
+        weekCard.append(dateContainer).append(temp).append(humidity).append(wind);
 
         weekEl.append(weekCard);
     }
